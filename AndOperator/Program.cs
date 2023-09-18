234a7866e2e78d5f7d29ff34c2c06ac5f19398a1
Marge K <marge.kaige@tktk.ee>
2023-09-18 16:13:46 +0300

    add week 5

---
+++ AndOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿// Math >=90; Biology >=90, Chemistry >=90
+
+int math, bio, chem;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter you Biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chem = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && bio >= 90 && chem >=90)
+{
+    Console.WriteLine("Congrants, you got accepted!");
+}
+else
+{
+    Console.WriteLine("Sorry, you didnt pass");
+}
---
+++ OrOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿// Math >=90; OR Chemistry >=90
+// math && chem || math && bio || chem && bio >90
+
+
+int math, bio, chem;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter you Biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chem = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chem >= 90) || (math >= 90 && bio >=90) || (chem >= 90 && bio >=90) )
+{
+    Console.WriteLine("Congrants, you got accepted!");
+}
+else
+{
+    Console.WriteLine("Sorry, you didn't pass");
+}
+
+
